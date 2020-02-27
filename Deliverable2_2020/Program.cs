0574f8653935d034b8b87b2937fbe7b24662198a
Kristy <harrisk26@gmail.com>
2020-02-27 09:41:51 -0500

    Second Deliverable

---
+++ Deliverable2_2020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Deliverable2_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message?");
+            string input = Console.ReadLine();
+            string upperInput = input.ToUpper();
+            string message = "";
+            int checksum = 0;
+            foreach (char c in upperInput)
+            {
+                int value = (System.Convert.ToInt32(c) - 64);
+                message += value.ToString() + "-";
+                checksum += (System.Convert.ToInt32(c));
+                }
+                Console.WriteLine("Your encoded message is " + message);
+                Console.WriteLine("Message checksum is " + checksum);
+            }
+            }
+        }
+    
